f47c1c2439780a4fd766b0e59d12a21b63efa7df
Alvin Muira <alvinmuira@gmail.com>
2025-07-13 21:50:21 +0300

    Finnal working

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     //Accessing the table body and form for the data to be be displayed
 const tableBody = document.getElementById("table-body")
 const editForm = document.getElementById("dog-form")
+const dogUrl = "http://localhost:3000/dogs";
     //Fetching the dog data from the server
-fetch(`http://localhost:3000/dogs`)
+function loadsDogs() {
+    fetch(`http://localhost:3000/dogs`)
     .then(response => response.json())
     .then(dogs => {
         dogs.forEach(dog => {
@@ -18,6 +20,8 @@ fetch(`http://localhost:3000/dogs`)
             tableBody.appendChild(row);
         });
     })
+};
+loadsDogs();
     //Adding event listener to the table body for edit button clicks
     tableBody.addEventListener(`click`, (event)=> {
         const row = event.target.closest("tr");
@@ -32,30 +36,30 @@ fetch(`http://localhost:3000/dogs`)
         updatingDetails(id);
     })
     //Updating the Server through the form submission
-//     function updatingDetails(id){ 
-//         editForm.addEventListener("submit", (event) => {
-//         event.preventDefault();
-//         fetch(`http://localhost:3000/dogs${id}`, {
-//             method: "PATCH",
-//             headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify({
-//                 name: editForm.name.value,
-//                 breed: editForm.breed.value,
-//                 sex: editForm.sex.value
-//             })
-//         })
-//         .then(response => response.json())
-//         .then(updatedDog => {
-//             const row = document.querySelector(`tr td.id:contains('${id}')`).closest("tr");
-//             row.querySelector(".name").textContent = updatedDog.name;
-//             row.querySelector(".breed").textContent = updatedDog.breed;
-//             row.querySelector(".sex").textContent = updatedDog.sex;
-//             editForm.reset();
-//         });
-//     });
-// }
+     function updatingDetails(id){ 
+        editForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const values = {
+                name: editForm.name.value,
+                breed: editForm.breed.value,
+                sex: editForm.sex.value
+            }
+        fetch(`${dogUrl}/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(values) 
+        })
+        .then(response => response.json())
+        .then(() => {
+            editForm.reset();
+            loadsDogs(); // Refresh list after update
+        });
+    });
+}
 
 
 })
+
+
